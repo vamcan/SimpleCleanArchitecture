81778d8856117ea21bd4cc4ee9a5aa8a6bc4caa6
Reza Ghasemi <reza.ghasemi@amily.de>
2022-12-03 23:52:49 +0100

    add Test Projcts

---
+++ MySimpleCleanArchitecture/SimpleCleanArchitecture.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SimpleCleanArchitecture.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ MySimpleCleanArchitecture/SimpleCleanArchitecture.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SimpleCleanArchitecture.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
