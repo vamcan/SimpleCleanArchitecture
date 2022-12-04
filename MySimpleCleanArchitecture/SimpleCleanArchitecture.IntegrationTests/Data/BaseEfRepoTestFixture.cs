40bad6d61de764738b8d94342689b5a3c938273d
Reza Ghasemi <reza.ghasemi@amily.de>
2022-12-04 20:44:24 +0100

    Integration Test

---
+++ MySimpleCleanArchitecture/SimpleCleanArchitecture.IntegrationTests/Data/BaseEfRepoTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleCleanArchitecture.Domain.Base;
+using SimpleCleanArchitecture.Infrastructure.EntityFramework;
+
+namespace SimpleCleanArchitecture.IntegrationTests.Data
+{
+    public class BaseEfRepoTestFixture<T> where T : class, IAggregateRoot
+    {
+        protected AppDbContext _dbContext;
+
+        public BaseEfRepoTestFixture()
+        {
+            var options = CreateNewContextOptions();
+           
+
+            _dbContext = new AppDbContext(options);
+        }
+        protected static DbContextOptions<AppDbContext> CreateNewContextOptions()
+        {
+            // Create a fresh service provider, and therefore a fresh
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseInMemoryDatabase("SimpleCleanArchitecture")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        protected EfRepository<T> GetRepository()
+        {
+            return new EfRepository<T>(_dbContext);
+        }
+    }
+}
---
+++ MySimpleCleanArchitecture/SimpleCleanArchitecture.IntegrationTests/Data/Order/AddOrder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleCleanArchitecture.Domain.ValueObjects;
+
+namespace SimpleCleanArchitecture.IntegrationTests.Data.Order
+{
+    public class AddOrder : BaseEfRepoTestFixture<Domain.Order.Order>
+    {
+        [Fact]
+        public async Task AddOrderShouldAddOrderInDb()
+        {
+            var description = "TestDescription";
+            var order = new Domain.Order.Order()
+            {
+                Description = description,
+                Email = new Email("reza@gmail.com"),
+                Title = "Title"
+            };
+            var repository = GetRepository();
+            await repository.AddAsync(order, CancellationToken.None);
+
+            var newOrder = (await repository.ListAsync()).FirstOrDefault();
+            Assert.Equal(newOrder.Description, description);
+
+        }
+    }
+}
